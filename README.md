7b84c2b765b0b4aaabc522356f8dbb99ecc521d1
RylanYan <rylan_yan@qq.com>
2016-12-04 21:25:40 +0800

    Public html-cache library.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# h5-cache
-cache for h5, support memeory/local storage/session storage/private mode
+# html5-cache:
+
+标签： html5
+
+---
+
+**针对html5单应用开发中经常使用的数据存储与缓存进行封装，包括：**
+ 
+ 1. 针对内存对象(memory object)，localStorage，sessionStorage，cookie等提供一致的api，如set,get，setAttr,getAttr等接口；
+ 2. 针对各种存储对象加入超时时间，支持缓存过期定期清除；
+ 3. 支持存储异常处理，针对memory/localStorage/sessioStorage/cookie等具有大小的存储介质，当超出存储时会按条件清除缓存，如FIFO/LRU等；
+ 4. 支持缓存降级处理，当使用localStorage/sessionStorage存储数据时，如果当前浏览器不支持或处于隐私模式，则自动降级采用memory存储，且支持刷新(使用window.name特性)
+
+**后期考虑**
+
+ 1. 对数据操作添加事件监听
+ 2. react方面：与react-router.state结合
+
+**该工具库在工作时使用，觉得复用性较高，便剥离出来与大家分享**
+ 
+
+
+
---
+++ demo/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import Cahce from '../src/index';
+const {
+  LocalCache,
+  SessionCache,
+  MemoryCache,
+} = Cahce;
+
+class DemoCache extends LocalCache {
+  constructor() {
+    const options = {
+      lifeTime: '2D', // 超时时间两天
+      defaultData: {
+        name: '',
+      },
+      key: 'demo_key',
+    };
+    super(options);
+  }
+}
+
+const demoCache = DemoCache.getInstance();
+const data = {
+  'name': 'yanjj',
+};
+demoCache.set(data);
+demoCache.setAttr('name', 'fanke');
+
+
+export default class HelloWorldComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    console.log('Hello html5-cache!');
+    return (
+      <div> Hello html5-cache! </div>
+    );
+  }
+}
+
+
+const rootInstance = ReactDom.render(
+  <HelloWorldComponent />,
+  document.getElementById('app')
+);
+
+if (module.hot) {
+  console.log('module.hot!')
+  require('react-hot-loader/Injection').RootInstanceProvider.injectProvider({
+    getRootInstances: function() {
+      // Help React Hot Loader figure out the root component instances on the page:
+      return [rootInstance];
+    },
+  });
+}
